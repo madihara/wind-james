4b8904c3d5ab4a3113df26545a8cd3df08e45c46
Madison Haradine <madisonharadine@gmail.com>
2020-02-11 16:33:25 -0500

    copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,35 +4,40 @@ import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
-      <div className={`${styles.box} ${styles.right}`}>
-        <h1 className={styles.title} >Company</h1>
-        <p>
-          word
+    <>
+      <div className={styles.container}>
+        <div className={`${styles.box} ${styles.right}`}>
+          <h1 className={styles.title} >Company</h1>
+          <p>
+            word
         </p>
-        <p>
-          hello
+          <p>
+            hello
         </p>
+        </div>
+        <div className={styles.box}>
+          <h1 className={styles.title} >Contact</h1>
+          <p>
+            (269)888-8888
+        </p>
+          <p>
+            email@email.com
+        </p>
+        </div>
+        <div className={styles.box}>
+          <h1 className={styles.title} >Locations</h1>
+          <p>
+            Eliza St
+        </p>
+          <p>
+            Schoolcradt, MI
+        </p>
+        </div>
       </div>
-      <div className={styles.box}>
-        <h1 className={styles.title} >Contact</h1>
-        <p>
-          (269)888-8888
-        </p>
-        <p>
-          email@email.com
-        </p>
-      </div>
-      <div className={styles.box}>
-        <h1 className={styles.title} >Locations</h1>
-        <p>
-          Eliza St
-        </p>
-        <p>
-          Schoolcradt, MI
-        </p>
-      </div>
+      <div className={styles.copyright}>
+        2020 Copyright &copy; All Rights Reserved By Wind + James
     </div>
+    </>
   )
 }
 
